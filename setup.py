56c4c670666438d7a9370c8cae271ad7c2987781
Casey Banner <kcbanner@gmail.com>
2009-04-28 17:19:45 -0400

    Added distutils setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='Kontagent',
+      version='0.1',
+      description='Kontagent Python client library',
+      author='Casey Banner',
+      author_email='casey.banner@kontagent.com',
+      url='http://www.kontagent.com/',
+      download_url='http://github.com/kontagent/kontagent-python-client-library/tree/master',
+      packages=['kontagent'],
+      )
+      
